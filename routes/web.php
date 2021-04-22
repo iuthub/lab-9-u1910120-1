11ce25120bc473bd9b808b36f94d748d53359ad1
u1910120 <s.gulamov@student.inha.uz>
2021-04-22 23:12:46 +0500

    lab 9

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,11 @@ Route::get('post/{id}', [
     'as' => 'blog.post'
 ]);
 
+Route::get('like/{id}', [
+    'uses' => 'PostController@getLikePost',
+    'as' => 'blog.post.like'
+]);
+
 Route::get('about', function () {
     return view('other.about');
 })->name('other.about');
@@ -44,6 +49,11 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('edit/{id}', [
         'uses' => 'PostController@getAdminEdit',
         'as' => 'admin.edit'
+    ]);
+
+    Route::get('delete/{id}', [
+        'uses' => 'PostController@getAdminDelete',
+        'as' => 'admin.delete'
     ]);
 
     Route::post('edit', [
